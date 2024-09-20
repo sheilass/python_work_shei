75496dffd53e495a86965ab4787f0edd4912adc8
43303 <43303@tjmt.jus.br>
2024-09-20 17:35:28 -0400

    :sparkles: feat: atribuindo dados a variaveis

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,17 @@
 print("Hello Python World!")
 print("Primeiro programa em Python - Aproveite a Viagem navegantes!")
 print("Aperte Crtl+F5 para executar")
+
+print("Variáveis no python")
+
+message = "Hello Python World! - adicionando primeira frase com atribuição a variável"
+print(message)
+
+message = "Hello Python World! - adicionando a primeira variável uma nova frase"
+print(message)
+
+message2 = "Hello Python Crash Course World! - segunda frase atribuída a uma nova variável"
+print(message2)
+
+message3 = "Juntando a segunda variável com a primeira temos:"
+print(message3, message, message2)
